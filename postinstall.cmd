c773afc9330923cdc3adbf829e50bff4d912a988
sam detweiler <sdetweil@gmail.com>
2025-03-06 09:45:32 -0700

    fix typo

--- postinstall.cmd
+++ postinstall.cmd
@@ -2,7 +2,7 @@ echo off
 setlocal enabledelayedexpansion
 set GH_PREFIX=https://github.com
 FOR /F "usebackq tokens=*" %%A IN (`powershell.exe -Command "('%PROCESSOR_ARCHITECTURE%').ToLower( )"`) DO SET arch=%%A
-echo arch=!arch!
+rem currently only amd64 built 3/6/25
 set arch=amd64
 for /f "delims=/ usebackq tokens=4,5,6" %%A in (`curl -sL %GH_PREFIX%/claudiodangelis/qrcp ^| findstr /C:"/claudiodangelis/qrcp/releases/tag/"`) do ( 
     set v=%%C
@@ -15,6 +15,7 @@ if exist xx.gz (
 	set qrcppath="%~dp0qrcp.exe"
 	@rem mklink !HOMEPATH!\AppData\Local\Microsoft\WindowsApps\qrcp.exe !qrcppath!	
     copy qrcp.exe !HOMEPATH!\AppData\Local\Microsoft\WindowsApps >nul
+	git checkout README.md >nul 2>&1
 )
 
 rem https://github.com/claudiodangelis/qrcp/releases/download/v0.11.4/qrcp_0.11.4_windows_amd64.tar.gz	
